cabb82fc783491e852b34717a5c8ffa8a264d4e2
Nicolas Seydoux <nseydoux@laas.fr>
2017-08-11 17:46:29 +0200

    Added configuration

--- src/main/java/fr/ocoma/Controler.java
+++ src/main/java/fr/ocoma/Controler.java
@@ -1,13 +1,55 @@
 package fr.ocoma;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.ocoma.config.Configuration;
 import fr.ocoma.endpoint.RESTServer;
 
 public class Controler {
 	
 	private static final Logger LOGGER = LogManager.getLogger(Controler.class);
+	
+	private static Controler instance;
+	private Configuration config;
+	
+	private Controler(File configFile){
+		ObjectMapper mapper = new ObjectMapper();
+        if(configFile != null){
+            try {
+                this.config = mapper.readValue(configFile, Configuration.class);
+            } catch (JsonParseException ex) {
+                LOGGER.error("Parsing failed");
+                ex.printStackTrace();
+            } catch (JsonMappingException e) {
+            	LOGGER.error("Mapping exception");
+				e.printStackTrace();
+			} catch (IOException e) {
+				LOGGER.error("IO exception");
+				e.printStackTrace();
+			}
+        }
+	}
+	
+	public Controler buildInstance(File configFile){
+		Controler.instance = new Controler(configFile);
+		return Controler.instance;
+	}
+	
+	public static Controler getInstance(){
+		return Controler.instance;
+	}	
+	
+	public Configuration getConfiguration(){
+		return this.config;
+	}
 	
 	public static void main(String[] args) {
 		LOGGER.info("Exécution du main !");
---
+++ src/main/java/fr/ocoma/config/Configuration.java
@@ -0,0 +1,13 @@
+package fr.ocoma.config;
+
+public class Configuration {
+	private String tripleStoreDescFile;
+
+	public String getTripleStoreDescFile() {
+		return tripleStoreDescFile;
+	}
+
+	public void setTripleStoreDescFile(String tripleStoreDescFile) {
+		this.tripleStoreDescFile = tripleStoreDescFile;
+	}
+}
