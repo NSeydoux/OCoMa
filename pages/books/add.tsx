bd45a8a2a330bdea82c350af9c06094073b7ae81
Nicolas Seydoux <nseydoux@inrupt.com>
2023-01-08 14:20:57 +0100

    Re-implement ISBN scan
    This implements a simple scan filling the form ISBN field reusing QuaggaJS for barcode scanning and detection.

--- pages/books/add.tsx
+++ pages/books/add.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
+import { useState } from 'react';
+import { BARCODE_CONTAINER_ID } from "../../src/lib/barcode";
+
+// The barcode component must be loaded client-side only
+const BarcodeReader = dynamic(
+  () => import("../../src/components/barcode"),
+  { ssr: false }
+);
 
 export default function Add() {
+  const [barcode, setBarcode] = useState<boolean>(false);
+  const [isbn, setIsbn ] = useState<number>(); 
   return (
     <>
       <Head>
@@ -12,10 +23,23 @@ export default function Add() {
       <main>
         <h1>Add a book to your library</h1>
         <Link href="/">Go back to homepage</Link>
+
         <form>
-          <label htmlFor="title">Title:</label>
-          <input type="text" name="title"></input>
+          <button type="button" onClick={() => setBarcode((prevState) => !prevState)}>
+            {barcode ? "End scan" : "Scan"}
+          </button>
+          <br/>
+          <label htmlFor="title">Title:
+            <input type="text" name="title"></input>
+          </label>
+          <br/>
+          <label htmlFor="isbn">ISBN:
+            <input type="text" name="isbn" value={isbn}></input>
+          </label>
         </form>
+        {/* The following must be present for the BarcodeReader component to anchor into. */}
+        <div id={BARCODE_CONTAINER_ID}></div>
+        <BarcodeReader enabled={barcode} onDetectedCallback={setIsbn}/>
       </main>
     </>
   )
---
+++ src/components/barcode.tsx
@@ -0,0 +1,15 @@
+import { runDetection } from "../lib/barcode"
+
+let scannerInterruptionCallback = () => {};
+
+const BarcodeReader: React.FC<{ enabled: boolean, onDetectedCallback: (code: number) => void }> = ({enabled, onDetectedCallback}): JSX.Element => {
+  if(enabled) {
+    scannerInterruptionCallback = runDetection(onDetectedCallback);
+  } else {
+    console.log("Scanning disabled")
+    scannerInterruptionCallback();
+  }
+  return <></>;
+}
+
+export default BarcodeReader
