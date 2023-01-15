5b936ea20faae20c6ac91a1dec2f71efb075c1e4
Zwifi <contact@zwifi.eu>
2023-01-15 16:05:57 +0100

    Add books to collection with basic form (#20)
    This adds a form to add books to your collection. The form includes book title, author, series and ISBN. Currently, when submitting the data, nothing happens.
    
    - Use formik to manage book submission
      - Use Formik to manage form state
      - Make barcode component not load any browser-side construct when pre-rendered
    - Support adding a list of authors
    - Add series field for book input: A book can now be tagged as part of a series.

--- pages/books/add.tsx
+++ pages/books/add.tsx
@@ -1,20 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react';
+import AddBookForm from '../../src/components/AddBookForm';
 import { getSession } from "../../src/lib/session";
-import { BARCODE_CONTAINER_ID } from "../../src/components/domConstants";
 
-// The barcode component must be loaded client-side only
-const BarcodeReader = dynamic(
-  () => import("../../src/components/barcode"),
-  { ssr: false }
-);
+
 
 export default function Add() {
-  const [barcode, setBarcode] = useState<boolean>(false);
-  const [isbn, setIsbn ] = useState<number>();
-
   useEffect(() =>  {
     getSession().handleIncomingRedirect({
       restorePreviousSession: true
@@ -31,23 +23,7 @@ export default function Add() {
       <main>
         <h1>Add a book to your library, {getSession().info.webId}</h1>
         <Link href="/">Go back to homepage</Link>
-
-        <form>
-          <button type="button" onClick={() => setBarcode((prevState) => !prevState)}>
-            {barcode ? "End scan" : "Scan"}
-          </button>
-          <br/>
-          <label htmlFor="isbn">ISBN:
-            <input type="text" name="isbn" value={isbn}></input>
-          </label>
-          <br/>
-          <label htmlFor="title">Title:
-            <input type="text" name="title"></input>
-          </label>
-        </form>
-        {/* The following must be present for the BarcodeReader component to anchor into. */}
-        <div id={BARCODE_CONTAINER_ID}></div>
-        <BarcodeReader enabled={barcode} onDetectedCallback={setIsbn}/>
+        <AddBookForm />
       </main>
     </>
   )
