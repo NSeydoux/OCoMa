b26494ca931aa7bc1945390e1def8ad0957fb005
Nicolas Seydoux <nseydoux@inrupt.com>
2023-08-28 22:16:05 +0200

    LintOC

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -3,13 +3,7 @@ import { RDF } from "@inrupt/vocab-common-rdf";
 import { SCHEMA } from "../lib/data/vocabConstants";
 import { rdfToBook } from "../lib/data/books";
 
-const listAuthors = (authors: string[]) => {
-
-}
-
 export default function BookList({library}: { library: SolidDataset }) {
-  // Tu en es là. D'abord, il faudrait modifier bookToRdf pour ajouter des types aux entités dans le graphe, 
-  // pour ensuite matcher sur ces types ici pour itérer sur les Things de type book.
   const books = getThingAll(library).filter(
     (thing) => getUrlAll(thing, RDF.type).includes(SCHEMA.classes.PublicationIssue)
   );
