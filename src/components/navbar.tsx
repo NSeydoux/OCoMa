cc4a4ed1c56f51696da5bab4d4bfb3d1b2d4959b
Nicolas Seydoux <nseydoux@inrupt.com>
2023-09-02 23:04:33 +0200

    Use deployment URL for redirect

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,36 @@
 import { useSession } from "@inrupt/solid-ui-react";
+import { GetServerSideProps } from "next";
 import Link from "next/link";
+
+export const getServerSideProps: GetServerSideProps<{
+  deployUrl: string
+}> = async () => {
+  return { props: { deployUrl: process.env.VERCEL_URL ?? "http://localhost:3000/" } }
+}
 
 // TODO: Make OP configurable.
 const OPENID_PROVIDER = "https://login.inrupt.com";
 
-const LoginButton = () => {
+const LoginButton = ({ deployUrl }: { deployUrl: string }) => {
   const { session } = useSession();
   if(!session.info.isLoggedIn) {
     return <button onClick={
       async () => await session.login({
         oidcIssuer: OPENID_PROVIDER,
         clientName: "OCoMa",
-        // redirectUrl: "http://localhost:3000/",
-        redirectUrl: window.location.href,
+        redirectUrl: deployUrl,
       })
     }>Login</button>
   }
   return <button onClick={async () => await session.logout()}>Logout</button>
 }
 
-export const NavBar = () => {
-  return <nav><LoginButton /> <Link href="/">Home</Link> <Link href="/add">Add</Link></nav>
+export const NavBar = ({ deployUrl }: { deployUrl: string }) => {
+  return (
+    <nav>
+      <LoginButton deployUrl={ deployUrl } />
+      <Link href="/">Home</Link>
+      <Link href="/add">Add</Link>
+    </nav>
+  );
 }
