12b9fcc30ed55387aff734543727047bc40e9630
Zwifi <contact@zwifi.eu>
2024-01-06 21:29:07 +0100

    Refactor book addition page

--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -10,16 +10,12 @@ export default function AddPage() {
   const {library } = useContext(LibraryContext);
   
   if (!session.info.isLoggedIn) {
-    return <div>Please log in to add a book to your library.</div>
+    return <p>Please log in to add a book to your library.</p>;
   }
 
-  return (
-    <div>
-      <h1>Add a book to your library, {session.info.webId}</h1>
-      { library !== undefined 
-        ? <AddBookForm/>
-        : <p>To be add books to your library, please define a library root first.</p>
-      }
-    </div>
-  )
+  if (library === undefined) {
+    return <p>To be add books to your library, please define a library root first.</p>;
+  }
+
+  return <AddBookForm/>;
 }
